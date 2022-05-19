1e53cf2a1329a763b0b7d623a6bcdbbb77ee1d6e
noceo <paulschade20@gmail.com>
2022-05-19 15:20:44 +0200

    updated deployment scripts

--- scripts/install_app_dependencies.sh
+++ scripts/install_app_dependencies.sh
@@ -3,3 +3,4 @@ cd /home/ec2-user/news-archive
 sudo npm i
 cd /home/ec2-user/news-archive/client
 sudo npm i
+exit 0
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+cd /home/ec2-user
 pm2 start ecosystem.config.js
+exit 0
