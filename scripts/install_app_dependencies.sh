5aead4149b57f1a8f996e36b12b4b8bcec56b614
noceo <paulschade20@gmail.com>
2022-05-23 13:07:13 +0200

    removed client dir from project

--- scripts/install_app_dependencies.sh
+++ scripts/install_app_dependencies.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 cd /home/ec2-user/news-archive
 sudo npm i
-cd /home/ec2-user/news-archive/client
-sudo npm i
-sudo npm run build
 exit 0
---
+++ scripts/stop-server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo pm2 stop all
+sudo pkill node
+exit 0
