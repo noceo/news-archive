40e349f2802721a3c1a6307313d4917691caaf88
noceo <paulschade20@gmail.com>
2022-06-26 11:59:25 +0200

    updated deploy script

--- scripts/install_app_dependencies.sh
+++ scripts/install_app_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/ec2-user/news-archive
 sudo npm i
-sudo cp /home/ec2-user/.env /home/ec2-user/news-archive
+sudo cp /home/ec2-user/.env /home/ec2-user/news-archive/.env
 exit 0
