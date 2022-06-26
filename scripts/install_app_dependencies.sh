6f18abd719ae57090f54b0783cf7ddb3386d705c
noceo <paulschade20@gmail.com>
2022-06-26 18:20:33 +0200

    updated deploy scripts

--- scripts/install_app_dependencies.sh
+++ scripts/install_app_dependencies.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /home/ec2-user/news-archive
-sudo npm i
-sudo cp /home/ec2-user/.env /home/ec2-user/news-archive/.env
+npm i
+cp /home/ec2-user/.env /home/ec2-user/news-archive/.env
+npx prisma migrate deploy
 exit 0
