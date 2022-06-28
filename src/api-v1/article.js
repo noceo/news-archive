d884b63a767c3c1cb71c8148365bc37979e7473e
noceo <paulschade20@gmail.com>
2022-06-28 14:59:39 +0200

    first draft of date range logic

--- src/api-v1/article.js
+++ src/api-v1/article.js
@@ -1,12 +1,35 @@
 import { Router } from 'express'
 import { PrismaClient } from '@prisma/client'
-import { NotFound } from '../errors'
+import ErrorType from '../errors'
 
 const prisma = new PrismaClient()
 const router = Router()
 
 router.get('/', async (req, res, next) => {
   try {
+    if (req.query.from && req.query.to) {
+      const from = new Date(req.query.from)
+      const to = new Date(req.query.to)
+      if (to < from) throw new Error('Invalid date range.')
+      const articles = await prisma.article.findMany({
+        where: {
+          published_at: {
+            gte: new Date(req.query.from),
+            lt: new Date(req.query.to),
+          },
+        },
+      })
+      console.log(articles)
+      if (articles.length > 0) {
+        articles.map((article) => delete article.publisher_id)
+        res.status(200).json(articles)
+        return
+      }
+      throw new Error(
+        `No articles found between ${req.query.from} and ${req.query.to}.`
+      )
+    }
+
     const articles = await prisma.article.findMany({
       include: {
         publisher: true,
@@ -18,14 +41,14 @@ router.get('/', async (req, res, next) => {
         },
       },
     })
-    if (articles) {
+    if (articles.length > 0) {
       articles.map((article) => delete article.publisher_id)
       res.status(200).json(articles)
       return
     }
     throw new Error('No articles found.')
   } catch (error) {
-    error.type = NotFound
+    error.type = ErrorType.NotFound
     next(error)
   }
 })
@@ -52,7 +75,7 @@ router.get('/:id', async (req, res, next) => {
     }
     throw new Error('The requested article could not be found.')
   } catch (error) {
-    error.type = NotFound
+    error.type = ErrorType.NotFound
     next(error)
   }
 })
