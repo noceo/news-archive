9c9a97052d8b71321e21852a8cb977a54f4fd02c
noceo <paulschade20@gmail.com>
2022-07-07 20:05:10 +0200

    added cors package

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 import api from './api-v1/index'
 import errorLogger from './middleware/error-logger'
 import errorResponder from './middleware/error-responder'
 import invalidPathHandler from './middleware/invalid-path-handler'
 
 const app = express()
+app.use(cors())
 app.use('/api/v1', api)
 app.use(errorLogger)
 app.use(errorResponder)
